4e212901690043c3067231b9b070c730927c0038
Marcelo Magrinelli <marcelovbm@gmail.com>
2020-06-14 13:24:12 -0300

    Inserindo morgan como log das requisições HTTP.

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
